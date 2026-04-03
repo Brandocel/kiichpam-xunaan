96a48d9971842a464740ace34fa859c4dab040a5
Brandocel <desarrollador@byhoka.com>
2026-04-03 15:25:44 -0500

    Fix

--- src/app/[locale]/pedidas-de-mano/page.tsx
+++ src/app/[locale]/pedidas-de-mano/page.tsx
@@ -1,9 +1,4 @@
 import ProposalPageView from "../../../features/pedidas-de-mano/components/ProposalPageView";
-import {
-  getProposalGallery,
-  getProposalHeroSlides,
-  getProposalPackages,
-} from "../../../features/pedidas-de-mano/services/proposal.service";
 
 interface ProposalPageProps {
   params: Promise<{
@@ -14,18 +9,5 @@ interface ProposalPageProps {
 export default async function ProposalPage({ params }: ProposalPageProps) {
   const { locale } = await params;
 
-  const [heroSlides, packageItems, galleryItems] = await Promise.all([
-    getProposalHeroSlides(),
-    getProposalPackages(locale),
-    getProposalGallery(locale),
-  ]);
-
-  return (
-    <ProposalPageView
-      heroSlides={heroSlides}
-      packageItems={packageItems}
-      galleryItems={galleryItems}
-      locale={locale}
-    />
-  );
+  return <ProposalPageView locale={locale} />;
 }
